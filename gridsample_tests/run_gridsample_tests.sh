e45921372b80731af205523086e061a30a33a08c
Christopher Melville <melvi083@csel-cuda-03.cselabs.umn.edu>
2025-12-18 21:52:52 -0600

    gridsample tests cleans up after itself now

--- gridsample_tests/run_gridsample_tests.sh
+++ gridsample_tests/run_gridsample_tests.sh
@@ -4,10 +4,17 @@ nvcc -O3 serial_testing.cu -o test_serial
 
 ./test_serial
 
+rm ./test_serial
+
 nvcc -O3 naive_parallel_testing.cu -o test_naive_parallel
 
 ./test_naive_parallel
 
+rm ./test_naive_parallel
+
 nvcc -O3 shared_parallel_testing.cu -o test_shared_parallel
 
 ./test_shared_parallel
+
+rm ./test_shared_parallel
+
